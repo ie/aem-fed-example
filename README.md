b440ea5cdccf835dd249869a9bfdce0e8609ef9b
Woo Ang <woo.ang@ie.com.au>
2019-05-02 14:31:16 +1000

    Initial 1 to 1 file set up

--- README.md
+++ README.md
@@ -2,3 +2,26 @@
 
 This project is about creating a means to separate AEM Back End Tech from Front End Development
 
+This is currently a 1 to 1 file generator from HTL to HTML
+
+The reference html file is meant to be stored in test/specs/*.spec.html
+
+The JSON object will be stored in test/specs/*.js
+
+The HTL file to edit is in test/templates/*.htl
+
+### To Start
+
+Need to create a symlink to the src folder
+
+> ln -s node_modules/@adobe/htlengine/src
+
+Then install packages with Yarn
+
+> yarn install
+
+### To Run
+
+Sample Run
+
+> node . test/templates/simple.htl
---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+// declared dependencies
+const fse = require('fs-extra');
+// local modules
+const engine = require('./node_modules/@adobe/htlengine/src/main');
+
+const resourceSimple = require('./test/specs/simple');
+
+(async () => {
+  const filename = process.argv[2];
+  const template = await fse.readFile(filename, 'utf-8');
+
+  const resource = resourceSimple;
+
+  engine(resource, template).then((ret) => {
+    // eslint-disable-next-line no-console
+    console.log(ret.body);
+  });
+})();
