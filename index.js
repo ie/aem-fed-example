adb1ebf1eb256396a9142ceb7eba6ace94b89662
Woo Ang <woo.ang@ie.com.au>
2019-05-07 17:28:51 +1000

    Add Webpack

--- index.js
+++ index.js
@@ -15,61 +15,61 @@ const path = require('path');
 // declared dependencies
 const fse = require('fs-extra');
 // local modules
-const engine = require('./src/main');
+const engine = require('./src/main'),
 
-// Define src folders - should move to .env folder
-const srcHTLFolder = './test/templates/';
-const srcSpecFolder = './test/specs/';
-const jsOutputFolder = './jsoutput/';
+  // Define src folders - should move to .env folder
+  srcHTLFolder = './test/templates/',
+  srcSpecFolder = './test/specs/',
+  jsOutputFolder = './jsoutput/';
 
 (async () => {
 
   fse.readdir(srcHTLFolder, async function (err, files) {
     if (err) {
-      console.error("Could not list the directory.", err);
+      console.error('Could not list the directory.', err);
       process.exit(1);
     }
-  
     files.forEach(async function (file, index) {
-      let filename = path.join(srcHTLFolder, file);
+      let filename = path.join(srcHTLFolder, file),
 
-      // Previous implementation from Node CLI
-      // const filename = process.argv[2];
+        // Previous implementation from Node CLI
+        // const filename = process.argv[2];
 
-      // New implementation which loops through the test folder
-      let fileshort = filename.substring(filename.lastIndexOf('/')+1);
-      let fileshorthtml = fileshort.replace(".htl", ".html");
-      let resourceFile = fileshorthtml.replace(".html", ".js");
+        // New implementation which loops through the test folder
+        fileshort = filename.substring(filename.lastIndexOf('/') + 1),
+        fileshorthtml = fileshort.replace('.htl', '.html'),
+        resourceFile = fileshorthtml.replace('.html', '.js');
       let resource = require(srcSpecFolder + resourceFile);
 
-      let template = await fse.readFile(filename, 'utf-8');
+
+      template = await fse.readFile(filename, 'utf-8');
 
       fse.stat(filename, async function (error, stat) {
         if (error) {
-          console.error("Error stating file.", error);
+          console.error('Error stating file.', error);
           return;
         }
-  
+
         if (stat.isFile()) {
           console.log("'%s' is a file to be processed.", filename);
 
           engine(resource, template, resourceFile).then((ret) => {
             // eslint-disable-next-line no-console
-            
+
             const filenameOut = path.resolve(process.cwd(), './generated_html/' + fileshorthtml);
             fse.writeFile(filenameOut, ret.body, 'utf-8');
 
             // Remove generated javascript files
             fse.unlinkSync(jsOutputFolder + resourceFile);
-            
+
             // Optional output html to console
             // console.log(ret.body);
-    
+
           });
         }
 
         else if (stat.isDirectory())
-          console.log("'%s' is a directory to be skipped.", filename);
+        {console.log("'%s' is a directory to be skipped.", filename);}
 
       });
     });
